9c64ab258b7aa09e7e45ddb60b3a4dc644106ec7
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-09-20 23:15:13 +0000

    playing with numbers

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        number = -number % 10
+    else:
+        number = number % 10
+
+    print(f"{number}", end='')
+    return number
