4aea037610fa075f60d02c6c0f1c20ba247641bd
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-09-20 18:36:06 +0000

    solved pycodestyke issue

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     l_digit = number % 10
 else:
-    last_digit = ((number * -1) % 10) * -1  # last digit
-if last_digit < 6 and last_digit != 0:
+    l_digit = ((number * -1) % 10) * -1  # last digit
+if l_digit < 6 and l_digit != 0:
     print(f"Last digit of {number} is {l_digit} and is less than 6 and not 0")
-elif last_digit == 0:
+elif l_digit == 0:
     print(f"Last digit of {number} is {l_digit} and is 0")
 else:
     print(f"Last digit of {number} is {l_digit} and is greater than 5")
