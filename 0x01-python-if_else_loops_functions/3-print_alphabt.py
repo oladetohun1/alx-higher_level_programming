bbc78a265930f070a2f92728c26d10f628a79a17
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-09-20 19:51:18 +0000

    not equal to

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(65, 91):
-    print("{}".format(chr(i)), end='')
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end='')
