fce87a0b9f9f619e30ecb82f00e53d1bc00cb5a2
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-11-30 08:42:34 +0000

    Hot fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
--- a script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
+-- a script that converts hbtn_0c_0 database to UTF8
+-- (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
 --You need to convert all of the following to UTF8:
 -- Database hbtn_0c_0
 -- Table first_table
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
--- a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+-- a script that creates a table second_table in the database 
+-- hbtn_0c_0 in your MySQL server and add multiples rows.
 --second_table description:
 -- id INT
 -- name VARCHAR(256)
