bbc78b2bf126c0f1dcf835b2e4196f3e1fd5b7db
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-12-19 20:57:25 +0000

    this problem was with ,y gitbash

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // A script that prints exactly  3 lines
 
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavaSscript is amazing');
