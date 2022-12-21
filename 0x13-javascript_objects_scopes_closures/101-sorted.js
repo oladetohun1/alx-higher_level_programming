a742f699bf45034bc5e6ac81cf00602b593954f0
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-12-21 02:42:59 +0000

    minor fixes

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-// function that returns the number of occurrences in a list
-
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map(function (e, index) {
-  return (e * index);
-});
-console.log(mappedList);
+const dict = require('./101-data').dict;
+console.log(Object.entries(dict).reduce(function (i, idx) {
+  i[idx[1]] = (i[idx[1]] || []).concat(idx[0]);
+  return i;
+}, {}));
