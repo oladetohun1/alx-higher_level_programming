4eb28c24e7498b7cb6a0d0213543fa4d4f9dffb3
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-11 20:53:12 +0100

    prints out the first argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // a script that prints the first argument passed to it:
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else {
+if (process.argv[2]) {
   console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
