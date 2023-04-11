cacbac6e88e5f633dbc9dcdd88b3a1abe06c3b9c
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-11 20:58:22 +0100

    interpolating strings

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 // a script that prints two arguments passed to it,
 // in the following format: “ is ”
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+console.log(`${arg1} is ${arg2}`);
