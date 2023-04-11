0bdcce554eb322abce9eb54eea69791ed30e34f7
root <root@DESKTOP-QH37QB.localdomain>
2023-04-11 20:10:43 +0100

    print out the first argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // a script that prints the first argument passed to it:
-
-if (process.argv[2] === undefined) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else {
+}
+else if (process.argv.length > 2) {
   console.log(process.argv[2]);
 }
