6c2693531953d48360fccad1270b765eefb9e581
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-12 18:01:54 +0100

    convert to another base

--- 0x13-javascript_objects_scopes_closures/10-main.js
+++ 0x13-javascript_objects_scopes_closures/10-main.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const converter = require('./10-converter').converter;
 
-let myConverter = converter(10);
+let myConverter = converter(10); 
 
-console.log(myConverter(2));
+console.log(myConverter(2)); 
 console.log(myConverter(12));
 console.log(myConverter(89));
 
--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that imports an array and computes a new array.
 
-const originalList = require('./100-data').list;
+const originalList = require('./100-data').list; 
 console.log(originalList);
 const mappedList = originalList.map(function (e, index) {
   return (e * index);
