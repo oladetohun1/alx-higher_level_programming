d97d6b8827f11f161ba5bd35c82199185b7f7dc5
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-12 22:59:37 +0100

    imports an array and computes a new one

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // script that imports an array and computes a new array.
 
-const originalList = require('./100-data').list; 
+const originalList = require('./100-data').list;
 console.log(originalList);
-const mappedList = originalList.map(function (e, index) {
-  return (e * index);
+const mappedList = originalList.map(function (element, index) {
+  return (element * index);
 });
 console.log(mappedList);
