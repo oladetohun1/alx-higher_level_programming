5b2756a1973566323d6f2890cc4ffb981017698a
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-18 10:03:33 +0100

    remove exe permission

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ from sys import argv
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost",
                          user=argv[1],
-                         password=argv[2],
+                         passwd=argv[2],
                          db=argv[3],
                          port=3306)
     cursor = db.cursor()
