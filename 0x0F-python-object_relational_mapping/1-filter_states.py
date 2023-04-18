04182e9cfb7a233e855b55cea8b885c06cdf5ca2
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-18 03:36:04 +0100

    print where first is N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-a script that lists all states with a name starting with N 
+a script that lists all states with a name starting with N
 (upper N) from the database hbtn_0e_0_usa
 '''
 import MySQLdb
