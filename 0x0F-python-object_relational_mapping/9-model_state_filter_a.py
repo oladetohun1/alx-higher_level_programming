061ffda34cfe8c4dfa4103943ff448881ec7bb8c
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-18 13:04:13 +0100

    contains a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -23,7 +23,5 @@ if __name__ == "__main__":
     if state_a:
         for state in state_a:
             print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
     # close session
     session.close()
