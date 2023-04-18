2ec48531c6bbb824f08e1c2e5521b3253572a1c7
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-18 13:30:24 +0100

    prints the id that is  for the state name passed as an argument from db

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+a script that prints the State object with the name
+passed as argument from the database
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # create engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    # create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # query
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+    session.close()
