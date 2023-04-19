3bcf7badb20dd17d46b2f66ebc9e467b9ff3e5a5
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-19 05:14:02 +0100

    relationship with states and city

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+a script that creates the State “California”
+with the City “San Francisco” from the database
+"""
+
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    # Create an engine that stores data in the local directory's
+    # sqlalchemy_example.db file.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    # Create all tables in the engine. This is equivalent to "Create Table"
+    # statements in raw SQL.
+    Base.metadata.create_all(engine)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Create a new State and City
+    new_state = State(name="California")
+    new_city = City(name="San Francisco")
+    new_state.cities.append(new_city)
+
+    # Add the new objects to the session, so they'll be stored
+    session.add(new_state)
+    session.add(new_city)
+
+    # commit the session to the database
+    session.commit()
+
+    # Close the session
+    session.close()
+
+    # Close the engine
+    engine.dispose()
