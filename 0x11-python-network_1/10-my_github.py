a35afe06580425fd7c71e574ba598933f6a614e5
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-30 00:22:46 +0100

    my ID

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get(url, auth=(username, password))
+    print(response.json().get("id"))
