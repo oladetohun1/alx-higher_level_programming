33b33bbb264af612826c7ab9d25e70c6cb35eef0
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-30 21:33:10 +0100

    update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash script that sends a DELETE request to the URL passed as the first argument and displays the body
+#Bash script that sends a DELETE request to the URL passed as the first argument and display the body
 curl -s $1 -X DELETE
