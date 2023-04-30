92ed00581fea6f32a1d82068b3c9a39ccf071818
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-04-30 21:09:53 +0100

    displays the body of the POST requests

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the body of the response of a curl POST request
+# Displays the body of the response of a curl POST requests
 curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
