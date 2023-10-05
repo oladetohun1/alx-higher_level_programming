a1c30786aa987a690adc865ab61f76af89d84a68
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-10-05 21:21:14 +0100

    read and write a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
+#!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
+if (process.argv.length > 2) {
+  fs.readFile(process.argv[2], (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.toString('utf-8'));
+    }
+  });
+}
