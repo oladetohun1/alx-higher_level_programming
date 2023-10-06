cc4fd7c1efeed0a8c3fddf3a56f2b094e6e0e3a1
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-10-06 21:09:24 +0100

    update 0-readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf8', (err, content) => {
-  console.log(err || content);
-});
+
+if (process.argv.length > 2) {
+  fs.readFile(process.argv[2], (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.toString('utf-8'));
+    }
+  });
+}
